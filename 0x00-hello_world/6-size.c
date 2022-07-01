50c216e3719ab181dbff5ac70e5d696117df2f6f
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-01 01:36:03 -0700

    8

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
        	printf("Size of an int: %lu byte(s)\n", sizeof(i));
        	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
        	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-       	printf("Size of a float: %lu byte(s)\n", sizeof());
+       	printf("Size of a float: %lu byte(s)\n", sizeof(f));
        	return (0);
 }
