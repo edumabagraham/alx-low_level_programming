5929a7bdce8fa3b92f0c8ace63b876c1715b893d
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-02 07:10:05 +0000

    Removed the variable that holds the last number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,17 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     /* your code goes there */
-    signed int last = n % 10;
     if (last > 5)
     {
-        printf("Last digit of %i is %i and is greater than 5", n, last);
+        printf("Last digit of %i is %i and is greater than 5", n, (n % 10));
     }
     else if (last == 0)
     {
-        printf("Last digit of %i is %i and is 0", n, last);
+        printf("Last digit of %i is %i and is 0", n, (n % 10));
     }
     else
     {
-        printf("Last digit of %i is %i and is less than 6 and not 0", n, last);
+        printf("Last digit of %i is %i and is less than 6 and not 0", n, (n % 10));
     }
     return (0);
 }
