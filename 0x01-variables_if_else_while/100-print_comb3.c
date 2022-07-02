12ae6fd3e82e4462dcef04168fa619687b1ecef2
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-02 09:48:00 +0000

    for

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-int first = '0';
-int second = '1';
-
-while (first <= '8')
+int first;
+int second;
+for (first = '0'; first <= '9'; first++)
 {
-while (second <= '9')
+for (second = '1'; second <= '9'; second++)
 {
 putchar(first);
 putchar(second);
@@ -20,9 +19,7 @@ if (first != '8' || second != '9')
 putchar(',');
 putchar(' ');
 }
-second++;
 }
-first++;
 }
 putchar('\n');
 return (0);
---
+++ 0x01-variables_if_else_while/while.c
@@ -0,0 +1,19 @@
+
+// int first = '0';
+// int second = '1';
+
+// while (first <= '8')
+// {
+// while (second <= '9')
+// {
+// putchar(first);
+// putchar(second);
+// if (first != '9' || second != '9')
+// {
+// putchar(',');
+// putchar(' ');
+// }
+// second++;
+// }
+// first++;
+// }putchar('\n');
