808ec86be04c27d3931d40b61422ca9c2c21e2ca
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-06 17:27:45 +0000

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,29 +9,37 @@
 
 void print_times_table(int n)
 {
-    int i, j;
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int product = (i * j);
-            if (product < 10)
-            {
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-
-            if (j != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+if (n > 0 && n < 15)
+{
+int i, j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int product = (i * j);
+if (product < 10 && j != 0)
+{
+_putchar(' ');
+_putchar(product + '0');
 }
+else if (product >= 10)
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+else
+{
+_putchar((product / 10) + '0');
+}
+
+if (j != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}    
+}
+}
+
