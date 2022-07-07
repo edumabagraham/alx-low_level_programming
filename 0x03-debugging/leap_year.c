5ccefc39269b5c23750c034ee748e9be3e36c6aa
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-07 19:58:10 +0000

    1-main.c

---
+++ 0x03-debugging/leap_year.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void leap(int n)
+{
+if ((n % 4 == 0 && n % 100 != 0) || (n % 4 == 0 && n % 100 == 0 && n % 400 == 0))
+{
+printf("%04d: LEAP\n", n);
+}
+else
+{
+printf("%04d: NOT LEAP\n", n);
+}
+}
