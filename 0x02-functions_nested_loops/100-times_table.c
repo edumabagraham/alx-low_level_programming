9ad8222d7d253b4ccf687eb846478c9ac8807039
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-07 05:31:20 +0000

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times
+ * print_times_table - Prints the timetable times
  * table
- *
+ * @n: Argument
  * Return: Nothing
  */
 
@@ -20,13 +20,22 @@ int product = (i * j);
 if (product < 10 && j != 0)
 {
 _putchar(' ');
+_putchar(' ');
 _putchar(product + '0');
 }
-else if (product >= 10)
+else if (product >= 10 && product < 100)
 {
+_putchar(' ');
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
+else if (product >= 100 && j != 0)
+{
+_putchar((product / 100) + '0');
+_putchar((product / 10) % 10 + '0');
+_putchar((product % 10) + '0');
+}
+
 else
 {
 _putchar((product / 10) + '0');
@@ -39,7 +48,8 @@ _putchar(' ');
 }
 }
 _putchar('\n');
-}    
 }
 }
+}
+
 
