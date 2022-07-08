bd920c64548b2c38439486b0a212a72c39dcd06d
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-08 01:58:16 +0000

    Done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 {
 int j = 0;
 
-while (j <= 10)
+while (j < 10)
 {
 int i = 0;
 while (i <= 14)
@@ -21,7 +21,6 @@ if (i >= 10)
 _putchar((i / 10) + '0');
 }
 _putchar((i % 10) + '0');
-
 i++;
 }
 _putchar('\n');
