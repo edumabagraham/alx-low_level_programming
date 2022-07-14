d40b8051c213201cf13c0db6d62a4d24627d57ea
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-14 10:21:06 +0000

    Compares the length of two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strcmp - Concantenates two strings
+ * @*s1 pointer to first value of string
+ * @*s2 pointer to first value of string
+ * @s1: first argument
+ * @s2: second argument
+ *
+ * Return: pointer to resulting string dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+/**Length of s1*/
+int s1_len = 0, s2_len = 0;
+while (s1[s1_len] != '\0')
+{
+s1_len++;
+}
+
+/**Length of s2*/
+while (s2[s2_len] != '\0')
+{
+s2_len++;
+}
+
+if (s1_len == s2_len)
+{
+return (0);
+}
+
+else if (s1_len < s2_len)
+{
+return (-15);
+}
+else
+{
+return (15);
+}
+}
+
+
