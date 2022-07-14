5f973f17ab3d1b14cf1d08ad0a6adc11fa0bb62c
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-14 22:40:59 +0000

    Eyo

--- 0x06-pointers_arrays_strings/strcmp.c
+++ 0x06-pointers_arrays_strings/strcmp.c
@@ -13,18 +13,30 @@
 
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 == *s2)
+
+/**Value of s1*/
+int value = 0, i = 0;
+
+while (s1[i] == s2[i] && s1[i] != '\0')
 {
-if (*s1 == '\0')
+i++;
+if (s1[i] == s2[i])
 {
-return (0);
-}
-s1++;
-s2++;
-}
-return (*s1 - *s2);
+value = 0;
 }
 
+else if (s1[i] < s2[i])
+{
+value = -15;
+}
+
+else
+{
+value = 15;
+}
+}
+return (value);
+}
 
 
 
