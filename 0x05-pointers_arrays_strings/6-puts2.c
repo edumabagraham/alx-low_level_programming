37f3a96c016b81f0b79d02dbfd4b7c71c4d11212
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-18 11:33:09 +0000

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,24 +4,24 @@
  * puts2 - prints every other character
  * of a string with the first character,
  * followed by a new line.
- * 
+ *
  * @str: argument name
- * 
+ *
  * Return - nothing
  */
 
 void puts2(char *str)
 {
-    int i = 0, j;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    j = i - 1;
-    while (j >= 0)
-    {
-        _putchar(s[j]);
-        j-2;
-    }
-    _putchar('\n');
+int i = 0, j = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+while (j < i)
+{
+_putchar(str[j]);
+j += 2;
+}
+_putchar('\n');
 }
