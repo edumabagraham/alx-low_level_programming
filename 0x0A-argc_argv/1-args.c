9d411b862d91007178e805c343bdf83ecb416b6d
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-25 11:44:25 +0000

    argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,7 +10,7 @@
  *
  * Return: 0 on success
  */
-int main(int argc, char *__attribute__((unused)) argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 printf("%d\n", (argc - 1));
 return (0);
