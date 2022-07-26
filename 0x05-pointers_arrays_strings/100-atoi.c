864cd3de9448689323753dcddd269053302d93d0
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-26 11:46:24 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * atoi - converts a string to an
+ * _atoi - converts a string to an
  * integer
  *
  * @s:string argument
@@ -10,4 +10,34 @@
 
 int _atoi(char *s)
 {
+int i, sign = 0, flag = 0, value = 0, digit;
+
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '-')
+{
+sign++;
+}
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - 48;
+if (sign % 2)
+{
+digit = -digit;
+}
+value = value * 10 + digit;
+flag = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+{
+break;
+}
+}
+}
+
+if (flag == 0)
+{
+return (0);
+}
+return (value);
 }
