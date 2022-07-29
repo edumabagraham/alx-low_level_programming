ed89727a24609c7f4c2960f65ca90f2d5ca3e5d2
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-07-29 18:40:51 +0000

    concatenate two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+
+int _strlen(char *str, unsigned int i);
+/**
+ * string_nconcat - concantenate two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: first n bytes of s2
+ *
+ * Return: pointer newly allocated space in
+ * memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int s1_len = _strlen(s1, 0) - 1;
+unsigned int s2_len = _strlen(s2, 0);
+unsigned int len = 0, i;
+char *s;
+
+if (s2_len == n)
+{
+len = s1_len + s2_len;
+}
+else
+{
+len = s1_len + n;
+}
+
+
+s = malloc(len *sizeof(*s));
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+s[i] = s1[i];
+}
+
+if (s2_len == n)
+{
+for (i = 0; s2[i] != '\0'; i++)
+{
+s[s1_len + i] = s2[i];
+}
+}
+else
+{
+for (i = 0; i < n ; i++)
+{
+s[s1_len + i] = s2[i];
+}
+}
+s[s1_len + i] = '\0';
+
+return (s);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @str: the string
+ * @i: the iterator
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str, unsigned int i)
+{
+if (str[i] == '\0')
+{
+return (1);
+}
+return (1 + _strlen(str, i + 1));
+}
