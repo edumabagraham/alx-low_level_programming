6071d0812ff8b367baa87a08a0f56d8ed8425ebd
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-01 12:08:37 +0000

    betty

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Prints the file name of the
+ * file that was compiled
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
