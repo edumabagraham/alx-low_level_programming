f99cb1d15eb285f81be476da5fb3009f80456235
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-01 00:14:42 +0000

    bettyyy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 
 
-/*int length(char *str, int i)
-{
-if (str[i] == '\0')
-{
-return (1);
-}
-return (1 + length(str, i + 1));
-}*/
 /**
  * _strdup - returns the copy of a
  * string in a new memory space.
@@ -30,8 +22,7 @@ while (str[str_len] != '\0')
 {
 str_len++;
 }
-/**printf("%d ohne recursion\n", str_len);
-printf("%d mit recursion\n", l);*/
+
 
 s = malloc(str_len *sizeof(char));
 
