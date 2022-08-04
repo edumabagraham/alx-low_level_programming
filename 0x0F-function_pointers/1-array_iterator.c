7ab699c935699a63de792ef2ba186957c5177bb6
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-04 11:42:22 +0000

    edge cas - check if the all the arguments are passed to the function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,14 +2,17 @@
  * array_iterator - executes function as
  * a parameter
  *
- * @param array
- * @param size
- * @param action
- * 
+ * @array: array to be iterated
+ * @size: size of array
+ * @action: function pointer
+ *
  */
 void array_iterator(int *array, int size, void (*action)(int))
 {
 int i;
+if (!array || !size || !action)
+return;
+
 for (i = 0; i < size; i++)
 {
 action(array[i]);
