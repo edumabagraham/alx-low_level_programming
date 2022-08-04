a2e777f6ee78de3e8de8d7598f5661e42d1aa027
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-04 11:31:46 +0000

    print the elements in an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,17 @@
-void array_iterator(int *array, size_t size, void (*action)(int));
+/**
+ * array_iterator - executes function as
+ * a parameter
+ *
+ * @param array
+ * @param size
+ * @param action
+ * 
+ */
+void array_iterator(int *array, int size, void (*action)(int))
+{
+int i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
