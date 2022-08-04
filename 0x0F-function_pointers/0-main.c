d5483b4f4b89c4dd491e25cd4aea20e5c87f33f0
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-04 00:29:44 +0000

    betty check

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -5,9 +5,8 @@
  * print_name_as_is - prints a name as is
  * @name: name of the person
  *
- * Return: Nothing 
+ * Return: Nothing.
  */
-
 void print_name_as_is(char *name)
 {
     printf("Hello, my name is %s\n", name);
@@ -17,39 +16,37 @@ void print_name_as_is(char *name)
  * print_name_uppercase - print a name in uppercase
  * @name: name of the person
  *
- * Return: Nothing 
+ * Return: Nothing.
  */
-
 void print_name_uppercase(char *name)
 {
     unsigned int i;
+
     printf("Hello, my uppercase name is ");
     i = 0;
     while (name[i])
     {
-       if (name[i] >= 'a' && name[i] <= 'z')
-       {
-        putchar(name[i] + 'A' - 'a');
-       }
-       else
-       {
-        putchar(name[i]);
-       }
-       i++;        
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
     }
 }
 
 /**
- * main - Check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
     print_name("Bob", print_name_as_is);
-    print_name("Bod Dylan", print_name_uppercase);
+    print_name("Bob Dylan", print_name_uppercase);
     printf("\n");
-
     return (0);
 }
