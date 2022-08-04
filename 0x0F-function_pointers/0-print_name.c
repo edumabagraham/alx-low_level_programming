676560cce908fa76376c22b834ce2ee4b0c92fd9
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-04 00:25:21 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - Prints a name
+ *
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
