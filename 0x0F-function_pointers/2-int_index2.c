ccea2f4bcdda90dcf064aae5fb62d902f7e99ef9
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-05 10:45:34 +0000

    included the function_pointers.h header

---
+++ 0x0F-function_pointers/2-int_index2.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array to be iterated
+ * @size: size of the array
+ * @cmp: pointer to the function to be used
+ * to compare values
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+/*
+ * check if all the parameters are
+ * have been provided.
+ */
+
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
+
+return(-1);
+}
+
+
+
---
+++ 0x0F-function_pointers/2-int_index_mine.c
@@ -0,0 +1,50 @@
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array to be iterated
+ * @size: size of the array
+ * @cmp: pointer to the function to be used
+ * to compare values
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, cmp_value, count = 0;
+
+/*
+ * check if all the parameters are
+ * have been provided.
+ */
+
+if (size <= 0)
+{
+return (-1);
+}
+
+else
+{
+for (i = 0; i < size; i++)
+{
+cmp_value = cmp(array[i]);
+if (cmp_value != 0)
+{
+count++;
+break;
+}
+}
+}
+
+/*
+*count != 0 ? (i = i) : (i = -1);
+*/
+if (count != 0)
+i = i;
+
+else
+i = -1;
+
+
+return (i);
+}
+
+
