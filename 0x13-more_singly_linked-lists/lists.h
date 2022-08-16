47c86b3b0612bc5d1e2eda02a7d4e6acae12351b
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-16 10:32:23 +0000

    more singly linked list

--- 0x13-more_singly_linked-lists/lists.h
+++ 0x13-more_singly_linked-lists/lists.h
@@ -19,5 +19,6 @@ int print_listint(const listint_t *h);
 int listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif
