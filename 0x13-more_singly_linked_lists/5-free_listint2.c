b1014c1622fde846bde95bc7e7404055032df88c
Gloria Edumaba Graham <edumabagrahamp@gmail.com>
2022-08-16 11:41:23 +0000

    Free list (head set to NULL)

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the list
+ *
+ * @head: head node
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+free(*head);
+
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+*head = NULL;
+}
