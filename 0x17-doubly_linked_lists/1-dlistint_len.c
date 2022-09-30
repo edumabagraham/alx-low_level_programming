142b6c700a0bd3cd4e984ea204dc3358db01e529
edumabagraham <edumabagrahamp@gmail.com>
2022-09-30 08:50:57 +0000

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the number of elements
+ * in a linked list
+ *
+ * @h: head node pointer
+ * Return: number of nodes in the list
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 size_t nodes = 0;
@@ -8,8 +16,8 @@ return (0);
 
 while (h)
 {
-nodes ++;
-h= h->next;
+nodes++;
+h = h->next;
 }
 return (nodes);
 }
